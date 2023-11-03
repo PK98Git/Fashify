34379ee2bf87cc807b4eb9f2af6db4886455cb00
PasinduK98 <pkbiz1998@gmail.com>
2023-11-03 13:51:28 +0530

    feat:update and Category

---
+++ client/src/components/Form/CategoryForm.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const CategoryForm = ({ handleSubmit, value, setValue }) => {
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label">
+            New Category Name
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Enter new Category"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
+        </div>
+
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </>
+  );
+};
+
+export default CategoryForm;
