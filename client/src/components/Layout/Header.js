490437d5e664dd0706dbace6eb79294d4896e913
PasinduK98 <pkbiz1998@gmail.com>
2023-11-09 10:33:55 +0530

    Add Category page to get items category wise

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -4,8 +4,11 @@ import { FcShop } from "react-icons/fc/";
 import { useAuth } from "../../context/auth";
 import toast from "react-hot-toast";
 import SearchInput from "../Form/SearchInput";
+import useCategory from "./../../hooks/useCategory";
+
 const Header = () => {
   const [auth, setAuth] = useAuth();
+  const categories = useCategory();
   const handleLogout = () => {
     setAuth({
       ...auth,
@@ -37,7 +40,7 @@ const Header = () => {
               Fashify
             </Link>
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-              <SearchInput/>
+              <SearchInput />
               <li className="nav-item">
                 <NavLink
                   to="/"
@@ -48,10 +51,28 @@ const Header = () => {
                   Home
                 </NavLink>
               </li>
-              <li className="nav-item">
-                <NavLink to="/category" className="nav-link">
-                  Category
-                </NavLink>
+              <li className="nav-item dropdown">
+                <Link
+                  className="nav-link dropdown-toggle"
+                  to={"/categories"}
+                  data-bs-toggle="dropdown"
+                >
+                  Categories
+                </Link>
+                <ul className="dropdown-menu">
+                  <li>
+                    <Link className="nav-link" to={"/categories"}>
+                      All Categories
+                    </Link>
+                  </li>
+                  {categories.map((c) => (
+                    <li>
+                      <Link className="nav-link" to={`/category/${c.slug}`}>
+                        {c.name}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
               </li>
               {!auth.user ? (
                 <>
@@ -80,7 +101,12 @@ const Header = () => {
                     </NavLink>
                     <ul className="dropdown-menu">
                       <li>
-                        <NavLink className="nav-link" to={`/dashboard/${auth.user?.role === 1 ? "admin":"user"}`}>
+                        <NavLink
+                          className="nav-link"
+                          to={`/dashboard/${
+                            auth.user?.role === 1 ? "admin" : "user"
+                          }`}
+                        >
                           Dashboard
                         </NavLink>
                       </li>
--- controllers/productController.js
+++ controllers/productController.js
@@ -1,5 +1,6 @@
 import express from "express";
 import productModel from "../models/productModel.js";
+import categoryModel from "../models/categoryModel.js";
 import fs from "fs";
 import slugify from "slugify";
 
@@ -299,11 +300,10 @@ export const searchProductController = async (req, res) => {
   }
 };
 
-
 //similar product
-export const relatedProductController = async (req,res) =>{
+export const relatedProductController = async (req, res) => {
   try {
-    const {pid,cid} =  req.params
+    const { pid, cid } = req.params;
     const products = await productModel
       .find({
         category: cid,
@@ -311,21 +311,43 @@ export const relatedProductController = async (req,res) =>{
           $ne: pid,
         },
       })
-      .select("-photo").limit(3).populate("category")
-      res.status(200).send({
-        success:true,
-        products,
-      })
+      .select("-photo")
+      .limit(3)
+      .populate("category");
+    res.status(200).send({
+      success: true,
+      products,
+    });
   } catch (error) {
-    console.log(error)
+    console.log(error);
     res.status(400).send({
-      success:false,
-      message:'error while getting related product',
-      error
-    })
+      success: false,
+      message: "error while getting related product",
+      error,
+    });
   }
-}
+};
 
+//product by category
+
+export const productCategoryController = async (req, res) => {
+  try {
+    const category = await categoryModel.findOne({ slug: req.params.slug });
+    const products = await productModel.find({ category }).populate("category");
+    res.status(200).send({
+      success: true,
+      category,
+      products,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).send({
+      success: false,
+      error,
+      message: "Error while getting products",
+    });
+  }
+};
 // export const mytest = async (req, res) => {
 //   try {
 //     console.log("mytest controller running");
