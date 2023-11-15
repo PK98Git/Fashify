9a2bd40d3039f8320390da0a4c80af61a64eae2c
PasinduK98 <pkbiz1998@gmail.com>
2023-11-15 21:59:15 +0530

    Production build for deploy

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ import dotenv from "dotenv";
 import morgan from "morgan";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoute.js";
-import categoryRoutes from './routes/categoryRoutes.js'
+import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
-
+import path from "path";
 
 // configure env
 dotenv.config();
@@ -22,19 +22,20 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "./client/build")));
 
 //routes
 app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1/category", categoryRoutes)
+app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
 //rest api
-app.get("/", (req, res) => {
-  res.send("<h1>Welcome to Fashify</h1>");
+app.use("*", function (req, res) {
+  res.sendFile(path(__dirname, "./client/build/index.html"));
 });
 
 //PORT
-const PORT =  8080;
+const PORT = 8080;
 
 //run listen
 app.listen(PORT, () => {
