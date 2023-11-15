d299c25f47eae7b90da445d82dc5be2e57df6218
PasinduK98 <pkbiz1998@gmail.com>
2023-11-15 09:32:47 +0530

    add admin orders UI page

--- client/src/pages/Admin/AdminOrders.js
+++ client/src/pages/Admin/AdminOrders.js
@@ -1,21 +1,96 @@
-import React from 'react'
-import AdminMenu from './../../components/Layout/AdminMenu';
-import Layout from '../../components/Layout/Layout';
+import React, { useState, useEffect } from "react";
+import AdminMenu from "./../../components/Layout/AdminMenu";
+import Layout from "../../components/Layout/Layout";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { useAuth } from "../../context/auth";
+import moment from "moment";
 
 const AdminOrders = () => {
+  const [status, setStatus] = useState([
+    "Not Process",
+    "Processing",
+    "Shipped",
+    "Delivered",
+    "Cancel",
+  ]);
+  const [changeStatus, setChangeStatus] = useState("");
+  const [orders, setOrders] = useState([]);
+  const [auth, setAuth] = useAuth();
+
+  const getOrders = async () => {
+    try {
+      const { data } = await axios.get("/api/v1/auth/all-orders");
+      setOrders(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    if (auth?.token) getOrders();
+  }, [auth?.token]);
+
   return (
-    <Layout title="All Orders Data">
-      {" "}
+    <Layout title={"All Orders Data"}>
       <div className="row">
         <div className="col-md-3">
-			<AdminMenu/>
-		</div>
+          <AdminMenu />
+        </div>
         <div className="col-md-9">
-			<h1 className='text-center'>All Orders</h1>
-		</div>
+          <h1 className="text-center">All Orders</h1>
+		              {orders?.map((o, i) => {
+              return (
+                <div className="border shadow">
+                  <table class="table">
+                    <thead>
+                      <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Status</th>
+                        <th scope="col">Buyer</th>
+                        <th scope="col">Date</th>
+                        <th scope="col">Payment</th>
+                        <th scope="col">Quantity</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr className="table-active">
+                        <td>{i + 1}</td>
+                        <td>{o?.status}</td>
+                        <td>{o?.buyer?.name}</td>
+                        <td>{moment(o?.createAt).fromNow()}</td>
+                        <td>{o?.payment.success ? "Success" : "Failed"}</td>
+                        <td>{o?.products?.length}</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                  <div className="container">
+                    {o?.products.map((p,i) => (
+                      <div className="row mb-2 p-3 flex-row card">
+                        <div className="col-md-4">
+                          <img
+                            src={`/api/v1/product/product-photo/${p._id}`}
+                            className="card-img-top"
+                            alt={p.name}
+                            width={50}
+                            height={"50px"}
+                          />
+                        </div>
+                        <div className="col-md-8">
+                          <h4>{p.name}</h4>
+                          <p>{p.description.substring(0, 30)}</p>
+                          <h4>Price : {p.price}</h4>
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              );
+            })}
+        </div>
       </div>
     </Layout>
   );
-}
+};
 
-export default AdminOrders
+export default AdminOrders;
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -6,6 +6,7 @@ import {
   forgotPasswordController,
   updateProfileController,
   getOrdersController,
+  getAllOrdersController,
 } from "../controllers/authController.js";
 import { requireSignIn, isAdmin } from "../middlewares/authMiddleware.js";
 
@@ -39,5 +40,8 @@ router.put('/profile',requireSignIn,updateProfileController);
 
 //orders
 router.get('/orders',requireSignIn,getOrdersController)
+
+//all orders
+router.get('/all-orders',requireSignIn,isAdmin,getAllOrdersController)
 
 export default router;
